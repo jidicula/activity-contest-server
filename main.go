30f682017793ff6de0905748d3ce8cf5baf68875
jidicula <johanan@forcepush.tech>
2021-05-19 20:35:29 -0400

    feat(models): Add models

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Contest struct {
+	gorm.Model
+	StartDate time.Time
+	EndDate   time.Time
+	Users     []User
+	Score     uint
+}
+
+type User struct {
+	gorm.Model
+	Username          string `gorm:"unique"`
+	Password          string
+	ActivitySummaries []ActivitySummary
+}
+
+type ActivitySummary struct {
+	gorm.Model
+	MovePercent     uint
+	ExercisePercent uint
+	StandPercent    uint
+}
+
+// score computes the score of an ActivitySummary.
+func (as ActivitySummary) score() uint {
+	return as.MovePercent + as.ExercisePercent + as.StandPercent
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	as := ActivitySummary{
+		MovePercent:     100,
+		ExercisePercent: 30,
+		StandPercent:    10,
+	}
+	u := User{
+		Username:          "test",
+		Password:          "password",
+		ActivitySummaries: []ActivitySummary{as},
+	}
+	c := Contest{
+		StartDate: time.Now(),
+		EndDate:   time.Now().Add(time.Hour * 24 * 7),
+		Users:     []User{u},
+		Score:     u.ActivitySummaries[0].score(),
+	}
+	fmt.Printf("ActivitySummary:\n  %+v\n\nUser:\n  %+v\n\nContest:\n  %+v\n", as, u, c)
 }
